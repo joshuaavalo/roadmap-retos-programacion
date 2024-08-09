30dcc92f3d5428a2b3c60cbdcf4284f78fc91df6
joshuaavalo <avalojoshua83@gmail.com>
2024-08-09 17:12:42 -0300

    exercise 00

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/joshuaavalo.js
@@ -0,0 +1,53 @@
+/*
+ * ¿Preparad@ para aprender o repasar el lenguaje de programación que tú quieras?
+ * - Recuerda que todas las instrucciones de participación están en el
+ *   repositorio de GitHub.
+ *
+ * Lo primero... ¿Ya has elegido un lenguaje?
+ * - No todos son iguales, pero sus fundamentos suelen ser comunes.
+ * - Este primer reto te servirá para familiarizarte con la forma de participar
+ *   enviando tus propias soluciones.
+ *
+ * EJERCICIO:
+ * - Crea un comentario en el código y coloca la URL del sitio web oficial del
+ *   lenguaje de programación que has seleccionado.
+ * - Representa las diferentes sintaxis que existen de crear comentarios
+ *   en el lenguaje (en una línea, varias...).
+ * - Crea una variable (y una constante si el lenguaje lo soporta).
+ * - Crea variables representando todos los tipos de datos primitivos
+ *   del lenguaje (cadenas de texto, enteros, booleanos...).
+ * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+ *
+ * ¿Fácil? No te preocupes, recuerda que esta es una ruta de estudio y
+ * debemos comenzar por el principio.
+ */
+
+// https://www.javascript.com/
+
+//Comentario en una linea es de esta forma
+
+/*
+Comentarios en multilinea se
+hace de 
+esta otra forma
+*/
+
+
+// tipos de variables
+var variable = "Variable en Javascript";
+let varLet = "variable"
+const varibaleCostante = "Variable constante";
+
+
+//tipos de datos
+const string = "texto";
+const numeroEntero = 200;
+const numeroDecimal = 1.09;
+const boleano = true;
+
+//imprimir
+console.log("Hola, Javascript");
+
+
+
+
